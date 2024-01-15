2c28af0570f82fba28b9edc5fd7663875e3281d2
Boris Skert <boris.skert@gmail.com>
2024-01-15 12:04:02 +0100

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/capitalcoordinatesservice/CapitalCoordinatesServiceApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.capitalcoordinatesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalCoordinatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalCoordinatesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borisskert/capitalcoordinatesservice/CapitalCoordinatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.capitalcoordinatesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalCoordinatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
